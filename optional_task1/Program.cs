a7fa8b313f6524604c172198ef9a659712147033
DimaDK <dk1434547@gmail.com>
2023-05-27 23:17:36 +0300

    add optional_task1

---
+++ optional_task1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача необязательная 1:
+// на входе целое или вещественное число,
+// надо удалить вторую цифру слева целой части этого числа.
+
+// 145 -> 15
+// 1 -> нет
+// 567,123 -> 57,123
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = num;
+int num3 = 1;
+int count = 1;
+int result = 1;
+int count2 = 1;
+
+while (num >= 10)
+{
+     num = num / 10;
+     count++;
+}
+
+if (count == 1)
+{
+    System.Console.WriteLine("Второй цифры нет...");
+    System.Console.WriteLine($"разрядность введеного числа {count}");
+    return;
+}
+
+else if (count == 2)
+{
+    result = num2 / 10;
+    System.Console.WriteLine($"{result}");
+    return;
+}
+
+else
+        while (count > count2+1)
+        {
+            num3 = num3 * 10 ;
+            count--;
+        }  
+         result = num2 % num3;
+         System.Console.WriteLine($"{num}{result}");
