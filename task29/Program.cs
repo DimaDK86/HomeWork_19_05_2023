31d31bdabc8fa4b77980ecb0ced8481b3a9257ac
DimaDK <dk1434547@gmail.com>
2023-06-03 20:44:47 +0300

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 99);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+        Console.Write($"{item} ");
+    System.Console.WriteLine();
+}
+
+
+Console.Clear();
+int[] array = new int[8];
+System.Console.WriteLine($"Случаный набор 8ми чисел от 0 до 99 ");
+FillArray(array);
+PrintArray(array);
